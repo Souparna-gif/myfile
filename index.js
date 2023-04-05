cbf066f87e07e96265af38de0b22c746bb261c41
AR009-code <64037720+AR009-code@users.noreply.github.com>
2023-04-05 14:38:14 +0530

    Update index.js
    replaced the location object with windows.open

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const downloadBtn = document.querySelector(".download-btn");
 const fileLink = "https://github.com/Souparna-gif/Myfiles/raw/main/ControlSystem.pdf";
 const initTimer = () => {
-    if(downloadBtn.classList.contains("disable-timer")) {
-        return location.href = fileLink;
-    }
     let timer = downloadBtn.dataset.timer;
     downloadBtn.classList.add("timer");
     downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
@@ -13,7 +10,7 @@ const initTimer = () => {
             return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
         }
         clearInterval(initCounter);
-        location.href = fileLink;
+        windows.open(fileLink,"_blanck");
         downloadBtn.innerText = "Your file is downloading...";
         setTimeout(() => {
             downloadBtn.classList.replace("timer", "disable-timer");
